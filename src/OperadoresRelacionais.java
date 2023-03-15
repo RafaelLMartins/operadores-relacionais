9bd344745a44fcd601b6d6e42a701a2f46bd8531
RafaelLMartins <rafaelluism@hotmail.com>
2023-03-15 14:36:14 -0300

    Exercicio de estudo sobre operadores relacionais.

---
+++ src/OperadoresRelacionais.java
@@ -0,0 +1,45 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int i1 = 20;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597l;
+        long l2 = 8997l;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1==i2: "+(i1==i2));
+        System.out.println("i1!=i2: "+(i1!=i2));
+        System.out.println("i1>i2: "+(i1>i2));
+        System.out.println("i1<=i2: "+(i1<=i2));
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+}
